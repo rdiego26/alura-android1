037a5c829a6efdae91f778df5f1c0b6ac9bfa203
rdiego26 <rdieg26@gmail.com>
2020-03-19 22:39:49 -0300

    LIST static data

--- agenda/app/src/main/java/me/diegoramos/agenda/MainActivity.kt
+++ agenda/app/src/main/java/me/diegoramos/agenda/MainActivity.kt
@@ -2,11 +2,19 @@ package me.diegoramos.agenda
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView: ListView = findViewById(R.id.activity_student_list)
+        val studentList = listOf("Aluno 1", "Aluno 2")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, studentList)
+
+        listView.adapter = adapter
     }
 }
